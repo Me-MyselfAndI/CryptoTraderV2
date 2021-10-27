06f422948a5638d5dc2396a70b54d89c18604a26
Grigorii Podoksik <h.podoksik@gmail.com>
2021-10-27 15:21:49 -0400

    - Fixed issue: Bid Pade coefficient was being discarded - Increased prediction time preiod - Decreased required delay to start debugging process

--- data_digger.py
+++ data_digger.py
@@ -5,17 +5,127 @@ from time import sleep
 import robin_stocks.robinhood as rs
 
 class DataDigger:
-    def __init__(self, file_name, email, password, asset_code, precision=2, dT=5, coef_depth=10):
+    def __init__(self, file_name, asset_code, prediction_delay, precision=2, dT=5, coef_depth=10):
         self.code = asset_code
         self.precision = precision
         self.dT = dT
         self.coef_depth=coef_depth
         self.file_name = file_name
+        self.prediction_delay = prediction_delay
 
-        totp = pyotp.TOTP("YHYPKMLAURGON3I2").now()
-        rs.login(email, password, mfa_code=totp)
+        # Needed to avoid recalculating factorials in computations of Taylor polynomial coefficients
+        self._factorials = [math.factorial(x) for x in range(self.coef_depth)]
 
-    def fill_data_table (self, num_trials, prediction_delay=20):
+
+        # most recent self.ask_prices and self.bid_prices
+        self.ask_prices, self.bid_prices = [], []
+
+        # Predictions by Pade approximation
+        self.ask_pade_predictions = [0 for i in range(self.coef_depth * 2 + 20)]
+        self.bid_pade_predictions = [0 for i in range(self.coef_depth * 2 + 20)]
+
+
+        # Initial number of predictions are very wrong, since they
+        # depend on having previously collected all the data
+        self._threshold_to_start_filling_table = 2 * self.coef_depth + 1
+
+        self.fill_data_table(self._threshold_to_start_filling_table)
+
+
+    def get_data_row(self, for_training=True):
+        # Get the prices
+        crypto_quote = rs.crypto.get_crypto_quote(self.code)
+        if for_training:
+            self.ask_prices.append(round(float(crypto_quote["ask_price"]), self.precision))
+            self.bid_prices.append(round(float(crypto_quote["bid_price"]), self.precision))
+        print(f"\tAsk Price: ${round(float(crypto_quote['ask_price']), self.precision)}\n"
+              f"\tBid Price: ${round(float(crypto_quote['bid_price']), self.precision)}")
+
+        # Functions that get ask and bid prices
+        # Needed since Pade requires a continious function, and the price cannot
+        # be obtained at non-integer indices of the list
+        def get_ask_price_at(x):
+            below, above = self.ask_prices[math.floor(x)], self.ask_prices[math.ceil(x)]
+            if math.ceil(x) == x:
+                return above
+            return (above - below) / (math.ceil(x) - math.floor(x)) * (x - math.floor(x)) + below
+
+        def get_bid_price_at(x):
+            below, above = self.bid_prices[math.floor(x)], self.bid_prices[math.ceil(x)]
+            if math.ceil(x) == x:
+                return above
+            return (above - below) / (math.ceil(x) - math.floor(x)) * (x - math.floor(x)) + below
+
+        sleep(self.dT)
+
+        if len(self.ask_prices) > self._threshold_to_start_filling_table + self.prediction_delay:
+            ask_taylor_coefs = [spm.derivative(get_ask_price_at, 0, dx=1.0, n=i, order=2 * i + 1) / self._factorials[i]
+                                for i in range(self.coef_depth)]
+            bid_taylor_coefs = [spm.derivative(get_bid_price_at, 0, dx=1.0, n=i, order=2 * i + 1) / self._factorials[i]
+                                for i in range(self.coef_depth)]
+            print(ask_taylor_coefs)
+            print(bid_taylor_coefs)
+
+            # Pade approximation is a fraction with polynomials in both enumerator and denominator
+            # Creating those polinomials for ask and bid prices
+            ask_pade_numerator, ask_pade_denominator = spi.pade(ask_taylor_coefs,
+                                                                math.floor((2 * self.coef_depth - 1) / 4),
+                                                                math.floor((2 * self.coef_depth) / 4))
+            bid_pade_numerator, bid_pade_denominator = spi.pade(bid_taylor_coefs,
+                                                                math.floor((2 * self.coef_depth - 1) / 4),
+                                                                math.floor((2 * self.coef_depth) / 4))
+
+            # Converting numerator and denominator of ask Pade approx. from polinomials to values at the required point
+            numerator_value = 0
+            for i in range(len(ask_taylor_coefs)):
+                numerator_value += ask_pade_numerator[i] * ((2 * self.coef_depth) ** i)
+
+            denominator_value = 0
+            for i in range(len(ask_taylor_coefs)):
+                denominator_value += ask_pade_denominator[i] * ((2 * self.coef_depth) ** i)
+            curr_ask_pade_prediction = numerator_value / denominator_value
+            if for_training:
+                self.ask_pade_predictions.pop(0)
+                self.ask_pade_predictions.append(curr_ask_pade_prediction)
+
+            # Converting numerator and denominator of bid Pade approx. from polinomials to values at the required point
+            numerator_value = 0
+            for i in range(len(bid_taylor_coefs)):
+                numerator_value += bid_pade_numerator[i] * ((2 * self.coef_depth) ** i)
+
+            denominator_value = 0
+            for i in range(len(bid_taylor_coefs)):
+                denominator_value += bid_pade_denominator[i] * ((2 * self.coef_depth) ** i)
+            curr_bid_pade_prediction = numerator_value / denominator_value
+            if for_training:
+                self.bid_pade_predictions.pop(0)
+                self.bid_pade_predictions.append(curr_bid_pade_prediction)
+
+            print(f"Pade approximations:\n\tAsk: ${curr_ask_pade_prediction}\n\tBid: ${curr_bid_pade_prediction}")
+
+            self.ask_prices.pop(0)
+            self.bid_prices.pop(0)
+
+            if for_training:
+                ask_return_data = [self.ask_prices[-self.prediction_delay - 1]]
+                ask_return_data.extend(self.ask_prices[:self.coef_depth-1])
+                bid_return_data = [self.bid_prices[-self.prediction_delay - 1]]
+                bid_return_data.extend(self.bid_prices[:self.coef_depth-1])
+                pade_predictions_return = [self.ask_pade_predictions[-self.prediction_delay], self.bid_pade_predictions[-self.prediction_delay]]
+            else:
+                ask_return_data = self.ask_prices[-self.coef_depth:]
+                bid_return_data = self.bid_prices[-self.coef_depth:]
+                pade_predictions_return = [self.ask_pade_predictions[-self.prediction_delay], self.bid_pade_predictions[-self.prediction_delay]]
+
+            result = ask_return_data + bid_return_data + pade_predictions_return
+            return result
+
+        print(f"\u001b[31mNOT ENOUGH PAST DATA TO GENERATE PADE APPROXIMATIONS. Run "
+              f"get_one_row() {self._threshold_to_start_filling_table + self.prediction_delay - len(self.ask_prices) + 1}"
+              f" more times to start getting predictions\u001b[0m")
+        return False
+
+    def fill_data_table (self, num_trials):
         self.data_file = open(self.file_name, 'w')
         self.data_file.write('ask')
         for i in range(self.coef_depth - 1):
@@ -27,117 +137,42 @@ class DataDigger:
 
         self.data_file.write("\task_pade\tbid_pade\n")
 
-        # Needed to avoid recalculating factorials in computations of Taylor polynomial coefficients
-        factorials = [math.factorial(x) for x in range(self.coef_depth)]
-
-        # most recent ask_prices and bid_prices
-        ask_prices, bid_prices = [], []
-
-        # Predictions by Pade approximation
-        ask_pade_predictions = [0 for i in range(self.coef_depth * 2 + 20)]
-        bid_pade_predictions = [0 for i in range(self.coef_depth * 2 + 20)]
-
-        # Initial number of predictions are very wrong, since they
-        # depend on having previously collected all the data
-        threshold_to_start_filling_table = 2 * self.coef_depth + 1
-
         # Start predictions
-        for trial in range(1, num_trials + threshold_to_start_filling_table + 1):
-            # Get the prices
-            crypto_quote = rs.crypto.get_crypto_quote(self.code)
-            ask_prices.append(round(float(crypto_quote["ask_price"]), self.precision))
-            bid_prices.append(round(float(crypto_quote["bid_price"]), self.precision))
-            print("\nPrices #", trial+1, "\n\tAsk Price: $", ask_prices[-1], "\n\tBid Price: $", bid_prices[-1], sep="")
-
-            # Find the Pade Approximation for the current price
-            curr_pade_approximation = {'ask': 0, 'bid': 0}
-            for shift in range(-2, 3):
-                curr_pade_approximation['ask'] += ask_pade_predictions[self.coef_depth + shift]
-                curr_pade_approximation['bid'] += bid_pade_predictions[self.coef_depth + shift]
-            curr_pade_approximation['ask'] /= 5
-            curr_pade_approximation['bid'] /= 5
-
-            print(f"Prediction difference:\n\tAsk: ${curr_pade_approximation['ask']-ask_prices[-1]}\n\tBid: ${curr_pade_approximation['bid']-bid_prices[-1]}")
-
-            # Functions that get ask and bid prices
-            # Needed since Pade requires a continious function, and the price cannot
-            # be obtained at non-integer indices of the list
-            def get_ask_price_at(x):
-                below, above = ask_prices[math.floor(x)], ask_prices[math.ceil(x)]
-                if math.ceil(x) == x:
-                    return above
-                return (above - below) / (math.ceil(x) - math.floor(x)) * (x - math.floor(x)) + below
-
-            def get_bid_price_at(x):
-                below, above = bid_prices[math.floor(x)], bid_prices[math.ceil(x)]
-                if math.ceil(x) == x:
-                    return above
-                return (above - below) / (math.ceil(x) - math.floor(x)) * (x - math.floor(x)) + below
-
-            #
-            if len(ask_prices) > threshold_to_start_filling_table + prediction_delay:
-                ask_taylor_coefs = [spm.derivative(get_ask_price_at, 0, dx=1.0, n=i, order=2*i+1) / factorials[i] for i in range(self.coef_depth)]
-                bid_taylor_coefs = [spm.derivative(get_bid_price_at, 0, dx=1.0, n=i, order=2*i+1) / factorials[i] for i in range(self.coef_depth)]
-                print(ask_taylor_coefs)
-                print(bid_taylor_coefs)
-
-                # Pade approximation is a fraction with polynomials in both enumerator and denominator
-                # Creating those polinomials for ask and bid prices
-                ask_pade_numerator, ask_pade_denominator = spi.pade(ask_taylor_coefs, math.floor((2 * self.coef_depth - 1) / 4),
-                                                                    math.floor((2 * self.coef_depth) / 4))
-                bid_pade_numerator, bid_pade_denominator = spi.pade(bid_taylor_coefs, math.floor((2 * self.coef_depth - 1) / 4),
-                                                                    math.floor((2 * self.coef_depth) / 4))
-
-
-                # Converting numerator and denominator of ask Pade approx. from polinomials to values at the required point
-                numerator_value = 0
-                for i in range(len(ask_taylor_coefs)):
-                    numerator_value += ask_pade_numerator[i] * ((2 * self.coef_depth) ** i)
-
-                denominator_value = 0
-                for i in range(len(ask_taylor_coefs)):
-                    denominator_value += ask_pade_denominator[i] * ((2 * self.coef_depth) ** i)
-                ask_pade_predictions.pop(0)
-                ask_pade_predictions.append(numerator_value / denominator_value)
-
-                # Converting numerator and denominator of bid Pade approx. from polinomials to values at the required point
-                numerator_value = 0
-                for i in range(len(bid_taylor_coefs)):
-                    numerator_value += bid_pade_numerator[i] * ((2 * self.coef_depth) ** i)
-
-                denominator_value = 0
-                for i in range(len(bid_taylor_coefs)):
-                    denominator_value += bid_pade_denominator[i] * ((2 * self.coef_depth) ** i)
-                bid_pade_predictions.pop(0)
-                bid_pade_predictions.append(numerator_value / denominator_value)
-
-                print(f"Pade approximations:\n\tAsk: ${ask_pade_predictions[-1]}\n\tBid: ${bid_pade_predictions[-1]}")
-
-
+        for trial in range(1, num_trials + self._threshold_to_start_filling_table + 1):
+            print (f"\nPrices #{trial + 1}")
+            last_row_data = self.get_data_row()
+            if last_row_data:
                 # Write everything into the data table
                 temp_counter = 1
-                self.data_file.write(str(ask_prices[-prediction_delay - 1]))
+                self.data_file.write(str(self.ask_prices[-self.prediction_delay - 1]))
                 for i in range(self.coef_depth - 1):
                     temp_counter += 1
-                    self.data_file.write('\t' + str(ask_prices[i]))
-                self.data_file.write("\t" + str(bid_prices[-prediction_delay - 1]))
+                    self.data_file.write('\t' + str(self.ask_prices[i]))
+                self.data_file.write("\t" + str(self.bid_prices[-self.prediction_delay - 1]))
                 temp_counter += 1
                 for i in range(self.coef_depth - 1):
                     temp_counter += 1
-                    self.data_file.write('\t' + str(bid_prices[i]))
-                self.data_file.write('\t' + str(ask_pade_predictions[-prediction_delay]) + '\t' + str(bid_pade_predictions[-prediction_delay]) + "\n")
+                    self.data_file.write('\t' + str(self.bid_prices[i]))
+                self.data_file.write('\t' + str(self.ask_pade_predictions[-self.prediction_delay]) + '\t' +
+                                     str(self.bid_pade_predictions[-self.prediction_delay]) + "\n")
                 temp_counter += 2
-
-                ask_prices.pop(0)
-                bid_prices.pop(0)
-
-            sleep(self.dT)
-
-
+                print(last_row_data)
+            ''' # MOVED ABOVE
+                self.ask_prices.pop(0)
+                self.bid_prices.pop(0)
+            sleep(self.dT)                
+            '''
         self.data_file.close()
 
+
 def main():
-    data_digger = DataDigger(r"temp_coefs.csv", input("Enter login email:\t"), input("Enter login password:\t"), 'BTC')
+    email = input("Enter login email:\t")
+    password = input("Enter login password:\t")
+
+    totp = pyotp.TOTP("YHYPKMLAURGON3I2").now()
+    rs.login(email, password, mfa_code=totp)
+
+    data_digger = DataDigger(r"temp_coefs.csv", asset_code='BTC', prediction_delay=10)
     data_digger.fill_data_table(1000)
 
 if __name__ == "__main__":
--- data_handler.py
+++ data_handler.py
@@ -1,12 +1,12 @@
 from tensorflow.keras import layers, Sequential
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD, Adagrad, Adam, Nadam, RMSprop
 from sklearn.model_selection import train_test_split, StratifiedKFold as KFold
 import random, pandas
+
 
 class ModelBuilder:
     """
     Creates a model builder
-
     :param file: address file with the CSV data table
     :param dimension: column to use as Y ("ask" or "bid")
     :param test_ratio: percentage of input rows split out for testing (not used in model training)
@@ -14,7 +14,7 @@ class ModelBuilder:
     def __init__(self, file, dimension, test_ratio=0.1):
         # Create a Pandas DataFrame to extract data from the CSV
         self.data_frame = pandas.read_csv(file, sep="\t")
-        self.data_frame = self.data_frame.iloc[:, :-1]
+        self.data_frame = self.data_frame.iloc[:-1, :]
         self.data_frame = self.data_frame.sample(frac=1).reset_index(drop=True)
 
         # Set the Y value to the "ask" and "bid" prices
@@ -22,24 +22,26 @@ class ModelBuilder:
         # Remove Y from the data_frame
         self.data_frame = self.data_frame.drop("ask", axis=1).drop("bid", axis=1)
         # Divide the data into the train and test data
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.data_frame.to_numpy(), y, test_size=test_ratio)
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.data_frame.to_numpy(), y,
+                                                                                test_size=test_ratio)
 
     """
     Compiles a particular number of models and selects the best 
     among them for prediction of the parameter
-    
+
     :param num_folds: Number of times to retry compiling and fitting the model
     :rtype: Keras.Sequential
     :return: Model for prediction of the parameter
     """
-    def make_model (self, num_folds=25):
+    def make_model(self, num_folds=10):
         # the following code creates several models and finds the best to avoid sticking inside local minima
         best_error = float("inf")
         all_errors = []
-        for attempt in range (num_folds):
-            curr_x_train, curr_x_test, curr_y_train, curr_y_test = train_test_split(self.x_train, self.y_train, test_size=0.1)
+        for attempt in range(num_folds):
+            curr_x_train, curr_x_test, curr_y_train, curr_y_test = train_test_split(self.x_train, self.y_train,
+                                                                                    test_size=0.2)
             # Create a model
-            neurons_per_layer = random.randint(round(curr_x_train.shape[1]/3), round(curr_x_train.shape[1]))
+            neurons_per_layer = random.randint(round(curr_x_train.shape[1] / 3), round(curr_x_train.shape[1]))
             model = Sequential([
                 layers.Dense(neurons_per_layer, input_dim=curr_x_train.shape[1], activation='relu'),
                 layers.Dense(neurons_per_layer, input_dim=neurons_per_layer, activation='relu'),
@@ -47,14 +49,41 @@ class ModelBuilder:
             ])
 
             # compile and fit the model
-            model.compile(loss='mean_squared_logarithmic_error', optimizer=SGD(), metrics=['mse'])
-            error = float('inf')
-            history = model.fit(curr_x_train, curr_y_train, epochs=10000,
-                                batch_size=min(1500, int(curr_y_train.size/2)), verbose=2)
+            model.compile(loss='mean_squared_logarithmic_error', optimizer=RMSprop(learning_rate=0.002),
+                          metrics=['mse'])
+            model.fit(curr_x_train, curr_y_train, epochs=300,
+                      batch_size=min(1500, int(curr_y_train.size / 2)), verbose=2)
+            # compile and fit the model
+            model.compile(loss='mean_squared_logarithmic_error', optimizer=RMSprop(learning_rate=0.001),
+                          metrics=['mse'])
+            model.fit(curr_x_train, curr_y_train, epochs=700,
+                      batch_size=min(1500, int(curr_y_train.size / 2)), verbose=2)
+            # compile and fit the model
+            model.compile(loss='mean_squared_logarithmic_error', optimizer=RMSprop(learning_rate=0.0005),
+                          metrics=['mse'])
+            model.fit(curr_x_train, curr_y_train, epochs=1200,
+                      batch_size=min(1500, int(curr_y_train.size / 2)), verbose=2)
+            # compile and fit the model
+            model.compile(loss='mean_squared_logarithmic_error', optimizer=SGD(learning_rate=0.1), metrics=['mse'])
+            model.fit(curr_x_train, curr_y_train, epochs=1000,
+                      batch_size=min(1500, int(curr_y_train.size / 2)), verbose=2)
+            model.compile(loss='mean_squared_logarithmic_error', optimizer=SGD(learning_rate=0.01), metrics=['mse'])
+            model.fit(curr_x_train, curr_y_train, epochs=3000,
+                      batch_size=min(1500, int(curr_y_train.size / 2)), verbose=2)
+            # compile and fit the model
+            model.compile(loss='mean_squared_logarithmic_error', optimizer=SGD(learning_rate=0.005), metrics=['mse'])
+            model.fit(curr_x_train, curr_y_train, epochs=3500,
+                      batch_size=min(1500, int(curr_y_train.size / 2)), verbose=2)
+            # compile and fit the model
+            model.compile(loss='mean_squared_logarithmic_error', optimizer=SGD(learning_rate=0.001), metrics=['mse'])
+            model.fit(curr_x_train, curr_y_train, epochs=4000,
+                      batch_size=min(1500, int(curr_y_train.size / 2)), verbose=2)
+
             # Test the model on the TESTING data and record the result
             result = model.predict(curr_x_test).tolist()
             result = [(result[i][0], curr_y_test.tolist()[i],
-                       abs(result[i][0] - curr_y_test.tolist()[i]) / (curr_y_test.tolist()[i]) if curr_y_test.tolist()[i] != 0 else 0) for i in range(len(curr_y_test))]
+                       abs(result[i][0] - curr_y_test.tolist()[i]) / (curr_y_test.tolist()[i])
+                       if curr_y_test.tolist()[i] != 0 else 0) for i in range(len(curr_y_test))]
 
             # Find the average error
             errors = []
@@ -62,11 +91,11 @@ class ModelBuilder:
                 errors.append(curr_res[-1])
                 print(curr_res[:-1], "\t\t", round(curr_res[-1], 6))
             errors.sort()
-            avg_error = sum(errors)/len(errors)
+            avg_error = sum(errors) / len(errors)
 
             # Print the errors and average
             print(errors)
-            print(f"Average Error - Trial #{attempt+1}:\t{round(avg_error, 8)}")
+            print(f"Average Error - Trial #{attempt + 1}:\t{round(avg_error, 8)}")
             # If the average error is better than the currently best average error, save this as a new best model
             if best_error > avg_error:
                 best_model_num = "model num currently omitted"
@@ -89,6 +118,7 @@ class ModelBuilder:
 
         return best_model
 
+
 if __name__ == "__main__":
     ask_model_builder = ModelBuilder(r"data.csv", 'ask', test_ratio=0.15)
     ask_model = ask_model_builder.make_model(num_folds=15)
@@ -98,11 +128,10 @@ if __name__ == "__main__":
     ask_errors = []
     for expected, resulting in zip(y_test, ask_results):
         print(expected, resulting)
-        ask_errors.append(abs((resulting - expected)/expected))
+        ask_errors.append(abs((resulting - expected) / expected))
 
     avg_ask_error = sum(ask_errors) / len(ask_errors)
-    median_ask_error = ask_errors[int(round(len(ask_errors)/2))]
-
+    median_ask_error = ask_errors[int(round(len(ask_errors) / 2))]
 
     bid_model_builder = ModelBuilder(r"data.csv", 'bid', test_ratio=0.15)
     bid_model = bid_model_builder.make_model(num_folds=15)
@@ -112,10 +141,10 @@ if __name__ == "__main__":
     bid_errors = []
     for expected, resulting in zip(y_test, bid_results):
         print(expected, resulting)
-        bid_errors.append(abs((resulting - expected)/expected))
+        bid_errors.append(abs((resulting - expected) / expected))
 
     avg_bid_error = sum(bid_errors) / len(bid_errors)
-    median_bid_error = bid_errors[int(round(len(bid_errors)/2))]
+    median_bid_error = bid_errors[int(round(len(bid_errors) / 2))]
 
     print("Average ask error:\t", avg_ask_error)
     print("Median ask error:\t", median_ask_error)
